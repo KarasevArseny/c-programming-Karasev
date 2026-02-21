19abe59835cd7480defc71b6cae566435ba7d372
KarasevArseny <karasevarseny@yandex.ru>
2026-02-21 12:41:05 +0300

    Add files via upload

---
+++ practice-1/cirlce.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+double radius, area;
+printf("Enter radius: ");
+scanf("%lf", &radius);
+area = 3.14159 * radius * radius;
+printf("Area: %.2lf\n", area);
+return 0;
+}
---
+++ practice-1/hello.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(void) {
+printf("Hello! My name is Arseny\nI am learning C programing");
+return 0;
+}
